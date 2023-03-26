cd2e5aaf2402b3b9e3127f0c3a44efc34c80a61d
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2023-03-25 21:11:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 #Pruebas para usar streamlit en cloud streamlit y usar bases de datos y registros
 import streamlit as st
+import pandas as pd
+import streamlit as st
 
 st.title('Prueba')
 st.text_area('Escribe')
+
+
+
+
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
